0ccc1c8e5183acf54374ffc6ff695666132c75e8
gregory <gredmyr@gmail.com>
2024-12-22 15:56:17 +0300

    feat: add order controller and service

--- backend/src/order/dto/order.dto.ts
+++ backend/src/order/dto/order.dto.ts
@@ -1,1 +1,41 @@
-//TODO реализовать DTO для /orders
+import {
+  IsArray,
+  IsEmail,
+  IsMobilePhone,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
+export class CreateOrderDto {
+  filmId: string;
+  userId: string;
+  seats: string;
+}
+
+export class TicketDTO {
+  @IsString()
+  film: string;
+  @IsString()
+  session: string;
+  @IsString()
+  daytime: string;
+  @IsString()
+  day: string;
+  @IsString()
+  time: string;
+  @IsNumber()
+  row: number;
+  @IsNumber()
+  seat: number;
+  @IsNumber()
+  price: number;
+}
+
+export class OrderDataDto {
+  @IsEmail()
+  email: string;
+  @IsMobilePhone()
+  phone: string;
+  @IsArray()
+  tickets: TicketDTO[];
+}
