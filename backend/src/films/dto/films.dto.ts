a6fd4aa0af9fdbfeed5aedd113c8b00c685faf8d
gregory <gredmyr@gmail.com>
2024-12-23 14:17:32 +0300

    create base app

--- backend/src/films/dto/films.dto.ts
+++ backend/src/films/dto/films.dto.ts
@@ -1,1 +1,27 @@
-//TODO описать DTO для запросов к /films
+export class GetScheduleDTO {
+  id: string;
+  daytime: string;
+  hall: number;
+  rows: number;
+  seats: number;
+  price: number;
+  taken: string[];
+}
+
+export class GetFilmDto {
+  id: string;
+  rating: number;
+  director: string;
+  tags: string[];
+  image: string;
+  cover: string;
+  title: string;
+  about: string;
+  description: string;
+  schedule: GetScheduleDTO[];
+}
+
+export class GetFilmsDto {
+  total: number;
+  items: GetFilmDto[];
+}
