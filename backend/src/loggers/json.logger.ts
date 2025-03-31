d87127a33d3b6a6fd549db346936cc7d47328e88
gregory <gredmyr@gmail.com>
2025-03-31 23:40:39 +0300

    Создали и продключили логгеры для пользователя, JSON и других машин

---
+++ backend/src/loggers/json.logger.ts
@@ -0,0 +1,33 @@
+import { LoggerService, Injectable, LogLevel } from '@nestjs/common';
+
+@Injectable()
+export class JsonLogger implements LoggerService {
+  error(message: any, ...optionalParams: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  warn(message: any, ...optionalParams: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  debug?(message: any, ...optionalParams: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  verbose?(message: any, ...optionalParams: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  fatal?(message: any, ...optionalParams: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  setLogLevels?(levels: LogLevel[]) {
+    throw new Error('Method not implemented.');
+  }
+  formatMessage(level: string, message: any, ...optionalParams: any[]) {
+    return JSON.stringify({ level, message, optionalParams });
+  }
+  /**
+   * Write a 'log' level log.
+   */
+  log(message: any, ...optionalParams: any[]) {
+    console.log(this.formatMessage('log', message, optionalParams));
+  }
+  // .....
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { TskvLogger } from './loggers/tskv.loggers';
+import { DevLogger } from './loggers/dev.loggers';
+import { JsonLogger } from './loggers/json.logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
+  app.useLogger(new DevLogger());
+  app.useLogger(new JsonLogger());
+  app.useLogger(new TskvLogger());
   await app.listen(3000);
 }
 bootstrap();
