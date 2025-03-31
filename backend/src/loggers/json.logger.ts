f24c2702f58494908990ed89a21918355c0a693a
gregory <gredmyr@gmail.com>
2025-04-01 00:01:01 +0300

    use prettir + linter + fix debug

--- backend/src/loggers/json.logger.ts
+++ backend/src/loggers/json.logger.ts
@@ -2,32 +2,36 @@ import { LoggerService, Injectable, LogLevel } from '@nestjs/common';
 
 @Injectable()
 export class JsonLogger implements LoggerService {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   error(message: any, ...optionalParams: any[]) {
     throw new Error('Method not implemented.');
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   warn(message: any, ...optionalParams: any[]) {
     throw new Error('Method not implemented.');
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   debug?(message: any, ...optionalParams: any[]) {
     throw new Error('Method not implemented.');
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   verbose?(message: any, ...optionalParams: any[]) {
     throw new Error('Method not implemented.');
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   fatal?(message: any, ...optionalParams: any[]) {
     throw new Error('Method not implemented.');
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setLogLevels?(levels: LogLevel[]) {
     throw new Error('Method not implemented.');
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   formatMessage(level: string, message: any, ...optionalParams: any[]) {
     return JSON.stringify({ level, message, optionalParams });
   }
-  /**
-   * Write a 'log' level log.
-   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   log(message: any, ...optionalParams: any[]) {
     console.log(this.formatMessage('log', message, optionalParams));
   }
-  // .....
 }
