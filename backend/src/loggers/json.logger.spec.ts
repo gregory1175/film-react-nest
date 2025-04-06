b8341cbcbf0c09dccae964d983abb8912a7273d7
gregory <gredmyr@gmail.com>
2025-04-06 21:48:56 +0300

    fix JsonLogger spec

--- backend/src/loggers/json.logger.spec.ts
+++ backend/src/loggers/json.logger.spec.ts
@@ -1,51 +1,24 @@
 import { JsonLogger } from './json.logger';
 
 describe('JsonLogger', () => {
-  let logger: JsonLogger;
+  let log: jest.SpyInstance<
+    void,
+    [message?: any, ...optionalParams: any[]],
+    any
+  >;
+  const jsonLogger = new JsonLogger();
 
   beforeEach(() => {
-    logger = new JsonLogger();
+    log = jest.spyOn(console, 'log').mockImplementation(() => {});
   });
 
   afterEach(() => {
-    jest.restoreAllMocks();
+    log.mockReset();
   });
 
-  test('should format message correctly', () => {
-    const formatted = logger.formatMessage('log', 'Test message', {
-      data: 123,
-    });
-    expect(formatted).toBe(
-      JSON.stringify({
-        level: 'log',
-        message: 'Test message',
-        optionalParams: [{ data: 123 }],
-      }),
-    );
-  });
-
-  test('should call console.log with formatted message', () => {
-    const spy = jest.spyOn(console, 'log').mockImplementation();
-    logger.log('Test message', { data: 123 });
-
-    expect(spy).toHaveBeenCalledWith(
-      JSON.stringify({
-        level: 'log',
-        message: 'Test message',
-        optionalParams: [{ data: 123 }],
-      }),
-    );
-  });
-
-  test('should throw error for unimplemented methods', () => {
-    expect(() => logger.error('Error message')).toThrow(
-      'Method not implemented.',
-    );
-    expect(() => logger.warn('Warning message')).toThrow(
-      'Method not implemented.',
-    );
-    expect(() => logger.debug?.('Debug message')).toThrow(
-      'Method not implemented.',
-    );
+  it('should log correct format', () => {
+    jsonLogger.warn('hi', { a: 'abc', c: 123 });
+    expect(log).toBeCalledTimes(1);
+    expect(log).toBeCalledWith(expect.stringContaining('"level":"warn"'));
   });
 });
